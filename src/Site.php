09d68b030bffde264135d3ecb76ceeac1e6f8973
Serge <serge.finciuc@incloud.com>
2018-06-23 00:47:25 +0300

    release

--- src/Site.php
+++ src/Site.php
@@ -34,6 +34,16 @@ class Site
         return "Hello World";
     }
 
+    /**
+     * Get a collection from the theme
+     *
+     * Always create a corresponding docblock for each method, describing what it is for,
+     * this helps the phpdocumentator to properly generator the documentation
+     *
+     * @param string $collectionName A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
+     *
+     * @return string
+     */
     public static function getCollection($collectionName)
     {
         return $collectionName;
