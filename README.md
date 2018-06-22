433e509bb7c21ff4276bf2e7207ea9ab42530666
Serge <serge.finciuc@incloud.com>
2018-06-23 00:18:49 +0300

    initial files

---
+++ README.md
@@ -0,0 +1,14 @@
+MONTE Site Integration
+=========================
+
+If you are trying to create a new MONTE Theme, this library will help you to make the communicating between a site and your them a lot easier and faster.
+
+Functions
+--------
+
+* \Monte\Site::getCollection()
+* \Monte\Site::getOptions()
+* \Monte\Site::getRow()
+
+
+More information and guides coming soon.
---
+++ src/Site.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Monte;
+
+/**
+ *  Site Class
+ *
+ *  Use this section to define what this class is doing, the PHPDocumentator will use this
+ *  to automatically generate an API documentation using this information.
+ *
+ * @author Sergiu Finciuc
+ */
+class Site
+{
+
+    private $base = '';
+
+    /**
+     * Sample method
+     *
+     * Always create a corresponding docblock for each method, describing what it is for,
+     * this helps the phpdocumentator to properly generator the documentation
+     *
+     * @param string $param1 A string containing the parameter, do this for each parameter to the function, make sure to make it descriptive
+     *
+     * @return string
+     */
+    public function hello($param1)
+    {
+        return "Hello World";
+    }
+}
