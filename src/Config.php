768db7d88fceb3da8c931b32170d69bc2ce375c5
Serge <serge.finciuc@incloud.com>
2018-07-02 16:20:19 +0300

    v.1

--- src/Config.php
+++ src/Config.php
@@ -7,6 +7,12 @@ use Monte\Resources\Api;
 class Config extends Api
 {
 
+    public static function settings()
+    {
+        return self::request('get', '/config/settings');
+    }
+
+
     public static function collections()
     {
         return self::request('get', '/config/collections');
@@ -15,6 +21,22 @@ class Config extends Api
     public static function options()
     {
         return self::request('get', '/config/options');
+    }
+
+
+    public static function hourly()
+    {
+        return self::request('get', '/config/hourly');
+    }
+
+    public static function daily()
+    {
+        return self::request('get', '/config/daily');
+    }
+
+    public static function monthly()
+    {
+        return self::request('get', '/config/monthly');
     }
 
 
